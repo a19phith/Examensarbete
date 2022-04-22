ef05603683b2964338bcfefb49b7bb858371844c
a19phith <62882844+a19phith@users.noreply.github.com>
2022-04-22 11:15:48 +0200

    Add files via upload

---
+++ phpmysql/mysql-search-experiment.php
@@ -0,0 +1,62 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "password";
+    $db = "phpmyadmin";
+
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $db);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    //timestamp array
+    $timestampList = [];
+
+    $month = rand (1, 3);
+    $day = rand (1, 31);
+
+    //How many times the get data iterates
+    for ($i=0; $i < 100 ; $i++) {
+        //Start time
+        $time_start = microtime(true);
+        //Get the row with the lowest EURMWh
+        //$sql = "SELECT * FROM elpriser WHERE EURMWh = (SELECT MIN(EURMWh) FROM elpriser)";
+        for ($t=0; $t < 50; $t++) {
+          $sql = "SELECT * FROM elpriser ORDER BY RAND () LIMIT 1; ";
+          $result = $conn->query($sql);
+
+          if ($result->num_rows > 0) {
+              // output data of each row
+              while($row = $result->fetch_assoc()) {
+                  echo "<br>". $row["distrikt"]. " ". $row["EURMWh"]. " " . $row["datum"] . " " . $row["startHour"]. " - " . $row["endHour"]."<br>";
+              }
+          } else {
+              echo "0 results";
+          }
+        }
+
+
+        $time_end = microtime(true);
+
+        //The time for the insert
+        $time = ($time_end - $time_start)*1000;
+
+        //Push in the time in the array
+        array_push($timestampList, $time);
+
+    }
+
+    //Create a CSV file
+    $file = fopen('Time-for-search-mysql.csv', 'w');
+    foreach ($timestampList as $line) {
+        //put data into csv file
+        fputcsv ($file, (array)$line);
+    }
+    fclose($file);
+    $conn->close();
+
+?>
